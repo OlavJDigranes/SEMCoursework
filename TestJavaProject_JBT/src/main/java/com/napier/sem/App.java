45dbc3afb880a5ef7bfa25414b680e78e1bb1add
OlavJDigranes <58626496+OlavJDigranes@users.noreply.github.com>
2021-02-03 14:01:45 +0000

    Network added. Dockerfile may need cleaning

--- TestJavaProject_JBT/src/main/java/com/napier/sem/App.java
+++ TestJavaProject_JBT/src/main/java/com/napier/sem/App.java
@@ -11,7 +11,9 @@ public class App {
     public static void main(String[] args)
     {
         // Connect to MongoDB on local system - we're using port 27000
-        MongoClient mongoClient = new MongoClient("localhost", 27000);
+        // MongoClient mongoClient = new MongoClient("localhost", 27000);
+
+        MongoClient mongoClient = new MongoClient("mongo-dbserver");
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
