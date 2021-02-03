4b26006559daad1ccebb71a3404228bb8401657e
MikhaRohrs <40437226@live.napier.ac.uk>
2021-02-03 12:46:51 +0000

    .

--- TestJavaProject_JBT/src/main/java/HelloWorld_JBT.java
+++ TestJavaProject_JBT/src/main/java/HelloWorld_JBT.java
@@ -1,5 +1,5 @@
 public class HelloWorld_JBT {
     public static void main(String[] args) {
-        System.out.print("Hello JBT! Hello Marco!");
+        System.out.print("Hello JBT! Hello Mikha!");
     }
 }
