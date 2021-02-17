97530a1be768f927249bea43eb5029871c6eaa94
Gianmarco Natuzzi <40439919@live.napier.ac.uk>
2021-02-17 11:23:31 +0000

    Fixing sql dockerfile

--- TestJavaProject_JBT/com/napier/sem/App.java
+++ TestJavaProject_JBT/com/napier/sem/App.java
@@ -16,7 +16,7 @@ public class App {
         // Connect to MongoDB on local system - we're using port 27000
         // MongoClient mongoClient = new MongoClient("localhost", 27000);
 
-        MongoClient mongoClient = new MongoClient("mongo-dbserver");
+        MongoClient mongoClient = new MongoClient("localhost", 27000);
         // Get a database - will create when we use it
         MongoDatabase database = mongoClient.getDatabase("mydb");
         // Get a collection from the database
