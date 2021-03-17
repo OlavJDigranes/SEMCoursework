bce2c2d0f378f2b0bad812f293be11e927b1dab8
Gianmarco Natuzzi <40439919@live.napier.ac.uk>
2021-03-17 13:54:33 +0000

    Added a Country class for the country table in sql.

---
+++ src/main/java/com/napier/sem/Country.java
@@ -0,0 +1,19 @@
+package com.napier.sem;
+
+public class Country {
+    public String code;
+    public String name;
+    public String continent;
+    public String region;
+    public double surfaceArea;
+    public int indepYear;
+    public int population;
+    public double lifeExpectancy;
+    public double GNP;
+    public double GNPOld;
+    public String localName;
+    public String govForm;
+    public String headOfState;
+    public int capital;
+    public String code2;
+}
