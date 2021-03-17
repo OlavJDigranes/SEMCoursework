b1b341746dfb338a11ba167bf83dd6ee4a06ebed
Gianmarco Natuzzi <40439919@live.napier.ac.uk>
2021-03-17 14:01:39 +0000

    Added a CountryLanguage class for the CountryLanguage table in sql.

---
+++ src/main/java/com/napier/sem/CountryLanguage.java
@@ -0,0 +1,8 @@
+package com.napier.sem;
+
+public class CountryLanguage {
+    public String countryCode;
+    public String language;
+    public boolean isOfficial;
+    public double percentage;
+}
